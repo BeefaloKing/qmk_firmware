7ce6932712b2a0a2d648ae5ac09c26ca6395ec99
BeefaloKing <9630438+BeefaloKing@users.noreply.github.com>
2020-07-07 19:23:44 -0600

    [Keymap] Beefalo - Add keymap to dz65rgb

---
+++ keyboards/dztech/dz65rgb/keymaps/beefalo/config.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "config_overrides.h"
+#include "config_rgb.h"
+
+/* Features
+ */
+#define FORCE_NKRO
+
+/* Configuration
+ */
+#define USB_POLLING_INTERVAL_MS 1
+#define DEBOUNCE 5
---
+++ keyboards/dztech/dz65rgb/keymaps/beefalo/rules.mk
@@ -0,0 +1,5 @@
+# Enable for Debugging
+CONSOLE_ENABLE = no
+
+# Uncomment to disable RGB
+# RGB_MATRIX_ENABLE = no
